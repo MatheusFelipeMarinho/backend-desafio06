b6163d87c5e8d97645af3c553727a4f7da26304f
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2020-11-12 13:37:44 -0300

    dev updates

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm';
+
+import category from './Category'
+
+@Entity('transactions')
 class Transaction {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => category)
+  @JoinColumn({name: 'category_id'})
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
